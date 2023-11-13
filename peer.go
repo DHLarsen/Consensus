a1513ea4b88e3b41ee99fb97f2b6293b1e19f88e
habr <habr@itu.dk>
2023-11-13 16:06:51 +0100

    more work on making peers communicate

--- peer.go
+++ peer.go
@@ -7,10 +7,12 @@ import (
 	"os"
 	"strings"
 	"sync"
+	"context"
 
 	gRPC "github.com/DHLarsen/ChittyChat/proto"
-	"google.golang.org/genproto/googleapis/rpc/context"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
 )
 
 type Peer struct {
@@ -18,17 +20,15 @@ type Peer struct {
 
 	name  string
 	port  string
-	mutex sync.Mutex
+	mutex sync.Mutex 
 }
 
 var port = ""
+var neighbor gRPC.ModelClient
 
 func (p *Peer) giveKey(ctx context.Context, key *gRPC.Key) (*gRPC.Ack, error){
-
-ack := &gRPC.Ack{
-	status: "1"
-	}
-return ack, nil
+	log.Printf("Recieved key")
+	return nil, nil
 }
 
 
@@ -57,21 +57,47 @@ func launchPeer() {
 			list = _list
 		}
 		
-	}	
-	var opts []grpc.ServerOption
-	grpcServer := grpc.NewServer(opts...)
+	}
+	grpcServer := grpc.NewServer()
 	peer := &Peer{
 		name: "PeerX",
 		port: port,
 	}
 	gRPC.RegisterModelServer(grpcServer, peer)
 	log.Println("Peer upstart sucessfull on port ", port)
+
+	if port == "8909" {
+		_neighbor, err := connectToPeer("8910")
+		if err != nil {
+			print(err)
+		} 
+		neighbor = _neighbor
+		ack, err := neighbor.GiveKey(context.Background(), &gRPC.Key{})
+		println("ack: ", ack)
+	}
 	if err := grpcServer.Serve(list); err != nil {
 		log.Fatalf("failed to serve %v", err)
 	}
 }
 
+func connectToPeer(_port string) (gRPC.ModelClient, error) {
+	opts := []grpc.DialOption{
+		grpc.WithBlock(),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	}
+	conn, err := grpc.Dial(":"+_port, opts...)
+	if err != nil {
+		print("Error in connect to peer", err)
+	} else {
+		log.Println("Connected to neighbor at port: ", _port)
+	}
+
+	return gRPC.NewModelClient(conn), nil
+}
+
 func main() {
-	launchPeer()
+	go launchPeer()
+	for {
+	}
 }
 
