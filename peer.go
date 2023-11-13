569ff6ea5d85c89fec1d741a8d1a4f170329fb5d
tpep <tpep@itu.dk>
2023-11-13 17:02:33 +0100

    Added TrimSpace in get_port and Big G

--- peer.go
+++ peer.go
@@ -1,18 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
 	"os"
 	"strings"
 	"sync"
-	"context"
 
 	gRPC "github.com/DHLarsen/ChittyChat/proto"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
-
 )
 
 type Peer struct {
@@ -20,19 +18,16 @@ type Peer struct {
 
 	name  string
 	port  string
-	mutex sync.Mutex 
+	mutex sync.Mutex
 }
 
 var port = ""
 var neighbor gRPC.ModelClient
 
-func (p *Peer) giveKey(ctx context.Context, key *gRPC.Key) (*gRPC.Ack, error){
+func (p *Peer) GiveKey(ctx context.Context, key *gRPC.Key) (*gRPC.Ack, error) {
 	log.Printf("Recieved key")
 	return nil, nil
 }
-
-
-
 
 func get_port(_i int) string {
 	body, err := os.ReadFile("ports.txt")
@@ -40,14 +35,23 @@ func get_port(_i int) string {
 		log.Fatalf("unable to read file: %v", err)
 	}
 	ports := strings.Split(string(body), "\n")
-	return ports[_i]
+
+	// Use strings.TrimSpace to remove leading and trailing whitespaces
+	return strings.TrimSpace(ports[_i])
 }
 
 func launchPeer() {
+	log.Println("2")
+
 	var list net.Listener
 	port_i := 0
+	log.Println("3")
+
 	for port == "" {
 		_port := get_port(port_i)
+		log.Println(_port + "before")
+		_port = strings.Trim(_port, " ")
+		log.Println(_port + "after")
 		_list, err := net.Listen("tcp", "localhost:"+strings.Trim(_port, " "))
 		if err != nil {
 			fmt.Println("failed to listen ", err)
@@ -56,7 +60,7 @@ func launchPeer() {
 			port = _port
 			list = _list
 		}
-		
+
 	}
 	grpcServer := grpc.NewServer()
 	peer := &Peer{
@@ -70,7 +74,7 @@ func launchPeer() {
 		_neighbor, err := connectToPeer("8910")
 		if err != nil {
 			print(err)
-		} 
+		}
 		neighbor = _neighbor
 		ack, err := neighbor.GiveKey(context.Background(), &gRPC.Key{})
 		println("ack: ", ack)
@@ -83,11 +87,12 @@ func launchPeer() {
 func connectToPeer(_port string) (gRPC.ModelClient, error) {
 	opts := []grpc.DialOption{
 		grpc.WithBlock(),
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithInsecure(),
 	}
 	conn, err := grpc.Dial(":"+_port, opts...)
 	if err != nil {
-		print("Error in connect to peer", err)
+		log.Println("Error connecting to peer:", err)
+		return nil, err
 	} else {
 		log.Println("Connected to neighbor at port: ", _port)
 	}
@@ -96,8 +101,8 @@ func connectToPeer(_port string) (gRPC.ModelClient, error) {
 }
 
 func main() {
+	log.Println("1")
 	go launchPeer()
 	for {
 	}
 }
-
